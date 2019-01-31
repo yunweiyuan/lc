28806d4dd4a60c983f84d4bb90206f8fc5f4cdca
yunweiyuan <yunwei.yuan@alumni.usc.edu>
2019-01-30 17:59:32 -0800

    update 114.flatten-binary-tree-to-linked-list.java

---
+++ 114.flatten-binary-tree-to-linked-list.java
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode id=114 lang=java
+ *
+ * [114] Flatten Binary Tree to Linked List
+ *
+ * https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/
+ *
+ * algorithms
+ * Medium (40.51%)
+ * Total Accepted:    213.7K
+ * Total Submissions: 526.2K
+ * Testcase Example:  '[1,2,5,3,4,null,6]'
+ *
+ * Given a binary tree, flatten it to a linked list in-place.
+ * 
+ * For example, given the following tree:
+ * 
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   5
+ * ⁠/ \   \
+ * 3   4   6
+ * 
+ * 
+ * The flattened tree should look like:
+ * 
+ * 
+ * 1
+ * ⁠\
+ * ⁠ 2
+ * ⁠  \
+ * ⁠   3
+ * ⁠    \
+ * ⁠     4
+ * ⁠      \
+ * ⁠       5
+ * ⁠        \
+ * ⁠         6
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+		return;
+	}
+
+	flatten(root.left);
+	flatten(root.right);
+
+	TreeNode leftTail = root.left;
+	while (leftTail != null && leftTail.right != null) {
+		leftTail = leftTail.right;
+	}
+
+	if (leftTail != null) {
+		leftTail.right = root.right;
+		root.right = root.left;
+		root.left = null;
+    	}
+    }
+}
